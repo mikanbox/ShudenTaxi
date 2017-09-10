639dbaebabb9a0be688a371033280ac432c4c213
佐藤輝年 <sato.geijo.13@gmail.com>
2017-09-10 13:39:59 +0900

    add MakeIdRequest and SettingConfirmRequest implementaion

--- Assets/Resources/Scripts/BackendWrapper/Request.cs
+++ Assets/Resources/Scripts/BackendWrapper/Request.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.Networking;
+using UniRx;
 
 public enum RequestStatus
 {
@@ -23,24 +26,80 @@ public abstract class Request
 
 public class MakeIDRequest : Request
 {
+	public static readonly string REQUEST_URL = BackendWrapperConf.APIURL + "/user/new";
+	private UnityWebRequest request;
+	private AsyncOperation webRequestStatus;
+	private System.IDisposable requestEvent;
+
 	public MakeIDRequest()
 	{
 		RequestSender.Instance.SubmitMakeIDRequest += OnSendRequest;
 	}
 
+	public class RequestAnswerData
+	{
+		public int userid;
+	}
+
 	private void OnSendRequest()
 	{
+		//前回のリクエストをが処理中の場合エラー
+		if (status == RequestStatus.Sending)
+		{
+			Debug.LogWarning("you cannnot sending request many times.Please wait previous request end");
+			return;
+		}
+
+		//リクエスト設定し、飛ばす。
+		request = UnityWebRequest.Get(REQUEST_URL);
+		webRequestStatus = request.Send();
 		status = RequestStatus.Sending;
-		status = RequestStatus.Success;
+
+		//リクエストの結果を受け取る。
+		requestEvent = this.ObserveEveryValueChanged(x => x.webRequestStatus.isDone)
+			.Where(isDone => isDone == true)
+			.Subscribe(isDone =>
+			{
+				if (request.isError)
+				{
+					Debug.Log(request.url);
+					Debug.LogError(request.error);
+					status = RequestStatus.Failure;
+				}
+				else
+				{
+					if (request.responseCode == 200)
+					{
+						string rawJson = request.downloadHandler.text;
+						RequestAnswerData answer = JsonUtility.FromJson<RequestAnswerData>(rawJson);
+						SetUserID(answer.userid);
+						status = RequestStatus.Success;
+					}
+					else
+					{
+						Debug.LogError("MakeUserIDRequest:API doesn't return responseCode 200");
+						status = RequestStatus.Failure;
+					}
+				}
+
+				if (requestEvent != null)
+					requestEvent.Dispose();
+			});
 	}
+
+	//Action
+	public System.Action<int> SetUserID;
 }
 
 public class MatchingRequest : Request
 {
+	public static readonly string REQUEST_URL = BackendWrapperConf.APIURL + "/matching/" + StateManager.Instance.userid;
+	private UnityWebRequest request;
+	private AsyncOperation webRequestStatus;
+	private System.IDisposable requestEvent;
 
 	public MatchingRequest()
 	{
-
 		RequestSender.Instance.SubmitMatchingRequest += OnSendRequest;
 	}
 
@@ -54,9 +113,62 @@ public class MatchingRequest : Request
 		public string comment;
 	}
 
+	public class AnswerData
+	{
+		public Comment[] commentList;
+		public bool isMatchingOK;
+		public string taxi_number;
+		public float? taxi_lat;
+		public float? taxi_lng;
+	}
+
 	private void OnSendRequest(RequestData data)
 	{
+		//前回のリクエストをが処理中の場合エラー
+		if (status == RequestStatus.Sending)
+		{
+			Debug.LogWarning("you cannnot sending request many times.Please wait previous request end");
+			return;
+		}
 
+		var dataAsDictionary = MiniJSON.Json.Deserialize(JsonUtility.ToJson(data)) as Dictionary<string,string>;
+
+		//リクエスト設定し、飛ばす。
+		request = UnityWebRequest.Post(REQUEST_URL,dataAsDictionary);
+		webRequestStatus = request.Send();
+		status = RequestStatus.Sending;
+
+		//リクエストの結果を受け取る。
+		requestEvent = this.ObserveEveryValueChanged(x => x.webRequestStatus.isDone)
+			.Where(isDone => isDone == true)
+			.Subscribe(isDone =>
+			{
+				if (request.isError)
+				{
+					Debug.Log(request.url);
+					Debug.LogError(request.error);
+					status = RequestStatus.Failure;
+				}
+				else
+				{
+					if (request.responseCode == 200)
+					{
+						string rawJson = request.downloadHandler.text;
+						var answer = MiniJSON.Json.Deserialize(rawJson) as Dictionary<string, string>;
+						
+						status = RequestStatus.Success;
+					}
+					else
+					{
+						Debug.LogError("MakeUserIDRequest:API doesn't return responseCode 200");
+						status = RequestStatus.Failure;
+					}
+				}
+
+				if (requestEvent != null)
+					requestEvent.Dispose();
+			
+			});
 	}
 }
 
@@ -75,12 +187,15 @@ public class SettingConfirmRequest : Request
 
 	public void OnSendRequest(SettingConfirmRequest.RequestData data)
 	{
-		
+		SetObjPostiion(data.obj_lat, data.obj_lng);
 	}
+
+	//Action
+	public System.Action<float, float> SetObjPostiion;
 }
 
 public class ChangeToSettingUIRequest : Request
-{ 
+{
 	public ChangeToSettingUIRequest()
 	{
 		RequestSender.Instance.SubmitChangeToSettingUIRequest += OnSendRequest;
@@ -93,22 +208,24 @@ public class ChangeToSettingUIRequest : Request
 		SetUIState(UIState.Setting);
 	}
 
+	//Action
 	public System.Action<UIState> SetUIState;
 }
 
 public class ChangeToCommentUIRequest : Request
-{ 
+{
 	public ChangeToCommentUIRequest()
 	{
 		RequestSender.Instance.SubmitChangeToCommentUIRequest += OnSendRequest;
 	}
 
-	public void OnSendRequest() 
-	{ 
+	public void OnSendRequest()
+	{
 		status = RequestStatus.Sending;
 		status = RequestStatus.Success;
-        SetUIState(UIState.Setting);
+		SetUIState(UIState.Setting);
 	}
 
+	//Action
 	public System.Action<UIState> SetUIState;
 }
--- Assets/Resources/Scripts/BackendWrapper/StateManager.cs
+++ Assets/Resources/Scripts/BackendWrapper/StateManager.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class StateManager : SingletonMonoBehaviour<StateManager>
 {
+	private const string PLAYER_PREFS_USER_ID_KEY = "userid";
+	private const string PLAYER_PREFS_OBJ_LAT_KEY = "obj_lat";
+	private const string PLAYER_PREFS_OBJ_LNG_KEY = "obj_lng";
+
 	//リクエストデータは取得不要だが、どこかでインスタンスを作らないといけないので、ここで保持
 	private MakeIDRequest makeIDRequest;
 	private MatchingRequest matchingRequest;
-	private SettingConfirmRequest settinfConfirmRequest;
+	private SettingConfirmRequest settingConfirmRequest;
 	private ChangeToSettingUIRequest changeToSettingUIRequest;
 	private ChangeToCommentUIRequest changeToCommentUIRequest;
 
 	//以下取得するもの
-	public int userid { get; private set; }
-	public float here_lat { get; private set; }
-	public float here_lng { get; private set; }
-	public float obj_lat { get; private set; }
-	public float obj_lng { get; private set; }
-	public float taxi_lat { get; private set; }
-	public float taxi_lng { get; private set; }
+	public int? userid { get; private set; }
+	public float? here_lat { get; private set; }
+	public float? here_lng { get; private set; }
+	public float? obj_lat { get; private set; }
+	public float? obj_lng { get; private set; }
+	public string taxi_number { get; private set;}
+	public float? taxi_lat { get; private set; }
+	public float? taxi_lng { get; private set; }
 	public Comment[] commentList { get; private set; }
 	public UIState uiState { get; private set; }
 	public MatchingState matchingState { get; private set; }
 
+	public RequestStatus makeIDRequestStatus { get { return makeIDRequest.status; } }
+	public RequestStatus matchingRequestStatus { get { return matchingRequest.status; } }
+	public RequestStatus settingConfirmRequestStatus { get { return settingConfirmRequest.status; } }
+	public RequestStatus changeToSettingUIRequestStatus { get { return changeToSettingUIRequest.status;} }
+	public RequestStatus changeToCommentUIRequestStatus { get { return changeToCommentUIRequest.status;} }
 
 	// Use this for initialization
 	protected override void Awake()
@@ -30,28 +40,56 @@ public class StateManager : SingletonMonoBehaviour<StateManager>
 		CheckInstance();
 		MakeRequestInstance();
 		SetRequestEvent();
+        LoadData();
+	}
+
+	private void Start() {
+		
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
+		
 	}
-
 
 	private void MakeRequestInstance()
 	{
 		makeIDRequest = new MakeIDRequest();
 		matchingRequest = new MatchingRequest();
-		settinfConfirmRequest = new SettingConfirmRequest();
+		settingConfirmRequest = new SettingConfirmRequest();
 		changeToSettingUIRequest = new ChangeToSettingUIRequest();
 		changeToCommentUIRequest = new ChangeToCommentUIRequest();
 	}
+
 	private void SetRequestEvent() {
+		makeIDRequest.SetUserID += (id) => {
+			userid = id;
+			PlayerPrefs.SetInt(PLAYER_PREFS_USER_ID_KEY, id);
+			PlayerPrefs.Save();
+		};
+		settingConfirmRequest.SetObjPostiion += (lat, lng) => { 
+			obj_lat = lat; obj_lng = lng;
+			PlayerPrefs.SetFloat(PLAYER_PREFS_OBJ_LAT_KEY, lat);
+			PlayerPrefs.SetFloat(PLAYER_PREFS_OBJ_LNG_KEY, lng);
+		};
 		changeToCommentUIRequest.SetUIState += (UIState obj) => uiState = obj;
 		changeToSettingUIRequest.SetUIState += (UIState obj) => uiState = obj;
 	}
 
+	private void LoadData()
+	{
+		bool hasUserID = PlayerPrefs.HasKey(PLAYER_PREFS_USER_ID_KEY);
+		if (hasUserID)
+			userid = PlayerPrefs.GetInt(PLAYER_PREFS_USER_ID_KEY);
+
+		bool hasObj = PlayerPrefs.HasKey(PLAYER_PREFS_OBJ_LAT_KEY) && PlayerPrefs.HasKey(PLAYER_PREFS_OBJ_LNG_KEY);
+		if (hasObj)
+		{
+			obj_lat = PlayerPrefs.GetFloat(PLAYER_PREFS_OBJ_LAT_KEY);
+			obj_lng = PlayerPrefs.GetFloat(PLAYER_PREFS_OBJ_LNG_KEY);
+		}
+	}
 }
 
 public class Comment
