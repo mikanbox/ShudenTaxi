8d734960b3c823f37b013288219f3565ad2a21a1
佐藤輝年 <sato.geijo.13@gmail.com>
2017-09-11 07:16:05 +0900

    add realtime realod comment implementation

---
+++ Assets/Resources/Scripts/BackendWrapper/GPSRunner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPSRunner : SingletonMonoBehaviour<GPSRunner> {
+
+	// Use this for initialization
+	void Start () {
+		Input.location.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.location.status == LocationServiceStatus.Running)
+		{
+			var lat = Input.location.lastData.latitude;
+			var lng = Input.location.lastData.longitude;
+			SetHereLocation(lat, lng);
+		}
+	}
+
+	public System.Action<float, float> SetHereLocation;
+}
