304590b4abe1c2885b8e429e377303848a1a8bf3
mikanbox <popoloas@yahoo.co.jp>
2017-09-10 09:18:29 +0900

    add UIManager_WindowChange and Creating_Map

--- Assets/Resources/Scripts/Frontend/CreatingMap.cs
+++ Assets/Resources/Scripts/Frontend/CreatingMap.cs
@@ -1,5 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+
+public class Positions{
+    public double Latitude;
+    public double Longitude;
+}
+
 
 public class CreatingMap : MonoBehaviour {
     GUITexture mapGuiTexture;
@@ -7,40 +14,103 @@ public class CreatingMap : MonoBehaviour {
     private int height = 800;
     private double longitudeMetor;
     private double latitudeMetor;
+    private int zoom = 13;          //半径3kmぐらい
+    private string AddComment;
+    private Positions GPS;
 
-    private int zoom = 6;
-    void Start () {
+    public static CreatingMap Instance;
+    void Awake()
+    {
+        Instance = (CreatingMap)this;
         mapGuiTexture = this.GetComponent<GUITexture> ();
-        //GetMap();
     }
 
-    void GetMap () {
-        StartCoroutine(GetStreetViewImage(45, 135, zoom));
-    }
+    public void GetMap () {
+        StartCoroutine (GetGPS());
+        GPS.Longitude = Input.location.lastData.longitude;
+        GPS.Latitude = Input.location.lastData.latitude;
 
+        AddComment = "";
+        // for (int i=0;i<StateManager.Instance.commentList.Length;i++){
+        //     AddComment += "&markers=size:mid%7Ccolor:blue%7C" + StateManager.Instance.commentList[i].lat + "," + StateManager.Instance.commentList[i].lng;
+        // }
+        StartCoroutine(GetStreetViewImage(GPS.Longitude, GPS.Latitude, zoom));
+    }
 
     private IEnumerator GetStreetViewImage(double latitude, double longitude, double zoom) {
-        　　　　　　　　//現在地マーカーはここの「&markers」以下で編集可能
-        string url = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=" + zoom + "&size=" + width + "x" + height + "&markers=size:mid%7Ccolor:red%7C" + latitude + "," + longitude;
+        //現在地マーカーはここの「&markers」以下で編集可能
+        string url = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=" + zoom + "&size=" + width + "x" + height +
+                     "&markers=size:mid%7Ccolor:red%7C" + latitude + "," + longitude + AddComment;
         WWW www = new WWW(url);
         yield return www;
-        　　　　　　　　//マップの画像をTextureとして貼り付ける
         mapGuiTexture.texture = www.texture;
-        　　　　　　　　//ミニマップに透明度を与える
         mapGuiTexture.color = new Color (mapGuiTexture.color.r, mapGuiTexture.color.g, mapGuiTexture.color.b, 0.4f);
     }
 
-    private void GetlatMeter(double latitude, double longitude) {
-        // laRe = (B.latitude - A.latitude)/M_PI * 180.0;
-        // loRe = (B.longitude - A.longitude)/M_PI * 180.0;
-        // longitudeMetor = earth_r * laRe;
-        // latitudeMetor = cos(deg2rad(A.latitude)) * earth_r * loRe;
+    private IEnumerator GetGPS() {
+        if (!Input.location.isEnabledByUser) {
+            yield break;
+        }
+        Input.location.Start();
+        int maxWait =  120;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0) {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+        if (maxWait < 1) {
+            print("Timed out");
+            yield break;
+        }
+        if (Input.location.status == LocationServiceStatus.Failed) {
+            print("Unable to determine device location");
+            yield break;
+        } else {
+            print("Location: " +
+                  Input.location.lastData.latitude + " " +
+                  Input.location.lastData.longitude + " " +
+                  Input.location.lastData.altitude + " " +
+                  Input.location.lastData.horizontalAccuracy + " " +
+                  Input.location.lastData.timestamp);
+        }
+        Input.location.Stop();
     }
 
-    private int getRealDistance(int metre) {
+
+    static double deg2rad(double deg)
+    {
+        return (deg / 180) * Math.PI;
+    }
+
+    public static int CalculateDistance(Positions posA, Positions posB)
+    {
+        // 2点の緯度の平均
+        double latAvg = deg2rad(posA.Latitude + ((posB.Latitude - posA.Latitude) / 2));
+        // 2点の緯度差
+        double latDifference = deg2rad(posA.Latitude - posB.Latitude);
+        // 2点の経度差
+        double lonDifference = deg2rad(posA.Longitude - posB.Longitude);
+
+        double curRadiusTemp = 1 - 0.00669438 * Math.Pow(Math.Sin(latAvg), 2);
+        // 子午線曲率半径
+        double meridianCurvatureRadius = 6335439.327 / Math.Sqrt(Math.Pow(curRadiusTemp, 3));
+        // 卯酉線曲率半径
+        double primeVerticalCircleCurvatureRadius = 6378137 / Math.Sqrt(curRadiusTemp);
+
+        // 2点間の距離
+        double distance = Math.Pow(meridianCurvatureRadius * latDifference, 2)
+                          + Math.Pow(primeVerticalCircleCurvatureRadius * Math.Cos(latAvg) * lonDifference, 2);
+
+        distance = Math.Sqrt(distance);
+
+        return (int)Math.Round(distance);
+    }
+
+
+
+    private int getRealDistance(int meter) {
         double equator = 40075334.2563;
         double disPerPix = equator / 256;
-        double disInPix = metre * (System.Math.Pow(2, zoom)) / disPerPix;
+        double disInPix = meter * (System.Math.Pow(2, zoom)) / disPerPix;
         return (int) disInPix;
     }
 
--- Assets/Resources/Scripts/Frontend/UIManager.cs
+++ Assets/Resources/Scripts/Frontend/UIManager.cs
@@ -1,16 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
 
-public class UIManager : MonoBehaviour {
+public class UIManager : SingletonMonoBehaviour<UIManager> {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]private InputField SendingMessage;
+    [SerializeField]private GameObject[] WindowObj;
+    [SerializeField]private GameObject StatusBar;
+
+    private UIState uiState = 0;
+
+    protected override void Awake()
+    {
+        CheckInstance();
+        StateManager.Instance.ObserveEveryValueChanged(x => x.uiState).Subscribe( _ => WindowChange());
+        StateManager.Instance.ObserveEveryValueChanged(x => x.matchingState).Subscribe( _ => StatusBarManager());
+    }
+
+    // public void Window_SettingOption() {
+    //     WindowChange(0, 4);
+    //     CreatingMap.Instance.GetMap();
+    // }
+
+    // public void Window_SendingMess() {
+    //     WindowChange(0, 1);
+    // }
+
+    // public void SeindingMessage() {
+    //     //string sendtxt = SendingMessage.text;
+    //     WindowChange(1, 2);
+    //     CreatingMap.Instance.GetMap();
+    //     StatusBarManager(true);
+    // }
+
+    // public void Window_ReachTaxi() {
+    //     WindowChange(2, 3);
+    // }
+
+    // public void Window_ReturnTopfromReachTaxi() {
+    //     WindowChange(3, 0);
+    // }
+
+    // public void Window_ReturnTopfromOption(bool Result) {
+    //     WindowChange(4, 0);
+    //     if (Result==true){
+    //         //StateManager.Instance.obj_lat=0;
+    //         //StateManager.Instance.obj_lng=0;
+    //     }
+    // }
+
+    public void StatusBarManager() {
+        StatusBar.SetActive((StateManager.Instance.matchingState == MatchingState.matching));
+    }
+
+    private void WindowChange() {
+        WindowObj[(int)uiState].SetActive(false);
+        WindowObj[(int)StateManager.Instance.uiState].SetActive(true);
+        uiState = StateManager.Instance.uiState;
+    }
 }
